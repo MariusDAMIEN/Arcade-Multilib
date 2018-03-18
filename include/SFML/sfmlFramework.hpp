931e36dbe58f47dadefbcb662287791841270cde
harold armijo leon <harold.armijo-leon@epitech.eu>
2018-03-18 17:22:00 +0000

    test sfml framework

--- include/SFML/sfmlFramework.hpp
+++ include/SFML/sfmlFramework.hpp
@@ -20,44 +20,62 @@
 class sfmlFramework : public IGraphic
 {
 public:
+	// sfmlFramework();
+	// ~sfmlFramework();
+	// // display all result at end and this is for update
+	// void displayOnWindow() override;
+	// // destroy all obj
+	// void destroy() override;
+	// void character() override;
+	// void wall() override;
+	// void score() override;
+	// void time() override;
+	// void characterName() override;
+	// void item() override;
+	// // init the window | done
+	// void createWindow(std::size_t, std::size_t, const std::string &) override;
+	// // open that winwow | done
+	// bool isOpenWindow() const override;
+	// // clear that window | done
+	// void clearWindow() override;
+	// // destroy that window
+	// void destroyWindow() override;
+	// // get the key pressed | done
+	// bool isKeyPressed(std::string) override;
+	// // draw a square | done
+	// IShape *drawRectangle(const std::size_t, const std::size_t, std::string, std::string) override;
+	// // template<typename T>
+	// void drawInBuff(IShape *shape, std::string type) override;
+
 	sfmlFramework();
 	~sfmlFramework();
-	// display all result at end and this is for update
-	void displayOnWindow() override;
-	// destroy all obj
-	void destroy() override;
-	void character() override;
-	void wall() override;
-	void score() override;
-	void time() override;
-	void characterName() override;
-	void item() override;
-	// init the window | done
-	void createWindow(std::size_t, std::size_t, const std::string &) override;
-	// open that winwow | done
-	bool isOpenWindow() const override;
-	// clear that window | done
-	void clearWindow() override;
-	// destroy that window
-	void destroyWindow() override;
-	// get the key pressed | done
-	bool isKeyPressed(std::string) override;
-	// draw a square | done
-	IShape *drawRectangle(const std::size_t, const std::size_t, std::string, std::string) override;
-	// template<typename T>
-	void drawInBuff(IShape *shape, std::string type) override;
-private:
-	void _strLower(std::string &);
-	IShape *_mySquare(const std::size_t, const std::size_t, sf::Color, sf::Color);
-	void _getRectangle();
+	bool createWindow(std::pair<int, int> size, std::string name) override;
+		// erease all of window
+	bool clearWindow() override;
+	bool createArea(std::pair<int, int> size, std::pair<int, int> pos,
+		std::string name, IGraphic::TYPE type) override;
+	bool loop(void (*func)(void)) override;
+	std::pair<int, int> getpos(std::string name) override;
+	bool setpos(std::pair<int, int> pos, std::string name) override;
+	std::pair<int, int> getdim(std::string name) override;
+	bool setdim(std::pair<int, int> dim, std::string name) override;
+	bool isKeyPressed(std::string key) override;
+	bool displayObj() override;
 
-	std::unique_ptr<sf::RenderWindow> _window;
-	sf::Event _event;
-	std::unordered_map<std::string, sf::Keyboard::Key> _key;
-	std::unordered_map<std::string, sf::Color> _colors;
-	std::unordered_map<std::string, std::function<void (void)> > _cast;
-	IShape *_myShape;
-	sf::RectangleShape _rectangle;
+private:
+	// void _strLower(std::string &);
+	// IShape *_mySquare(const std::size_t, const std::size_t, sf::Color, sf::Color);
+	// void _getRectangle();
+
+	// std::unique_ptr<sf::RenderWindow> _window;
+	// sf::Event _event;
+	// std::unordered_map<std::string, sf::Keyboard::Key> _key;
+	// std::unordered_map<std::string, sf::Color> _colors;
+	// std::unordered_map<std::string, std::function<void (void)> > _cast;
+	// IShape *_myShape;
+	// sf::RectangleShape _rectangle;
+
+
 };
 
 // extern "C" void libsf()
--- lib/SFML/sfmlFramework.cpp
+++ lib/SFML/sfmlFramework.cpp
@@ -11,180 +11,210 @@
 #include <cstddef>
 #include "SFML/sfmlFramework.hpp"
 
+// sfmlFramework::sfmlFramework()
+// {
+// 	std::cout << "framwork sfml creating" << std::endl;
+// 	_key = {
+// 		{"a", sf::Keyboard::A},
+// 		{"b", sf::Keyboard::B},
+// 		{"c", sf::Keyboard::C},
+// 		{"d", sf::Keyboard::D},
+// 		{"e", sf::Keyboard::E},
+// 		{"f", sf::Keyboard::F},
+// 		{"g", sf::Keyboard::G},
+// 		{"h", sf::Keyboard::H},
+// 		{"i", sf::Keyboard::I},
+// 		{"j", sf::Keyboard::J},
+// 		{"m", sf::Keyboard::M},
+// 		{"n", sf::Keyboard::N},
+// 		{"o", sf::Keyboard::O},
+// 		{"p", sf::Keyboard::P},
+// 		{"q", sf::Keyboard::Q},
+// 		{"r", sf::Keyboard::R},
+// 		{"s", sf::Keyboard::S},
+// 		{"t", sf::Keyboard::T},
+// 		{"u", sf::Keyboard::U},
+// 		{"v", sf::Keyboard::V},
+// 		{"w", sf::Keyboard::W},
+// 		{"x", sf::Keyboard::X},
+// 		{"y", sf::Keyboard::Y},
+// 		{"z", sf::Keyboard::Z},
+// 		{"escape", sf::Keyboard::Escape},
+// 		{"left", sf::Keyboard::Left},
+// 		{"right", sf::Keyboard::Right},
+// 		{"up", sf::Keyboard::Up},
+// 		{"down", sf::Keyboard::Down}
+// 	};
+// 	_colors = {
+// 		{"black", sf::Color::Black},
+// 		{"white", sf::Color::White},
+// 		{"red", sf::Color::Red},
+// 		{"green", sf::Color::Green},
+// 		{"blue", sf::Color::Blue},
+// 		{"yellow", sf::Color::Yellow},
+// 		{"magenta", sf::Color::Magenta},
+// 		{"cyan", sf::Color::Cyan},
+// 		{"transparent", sf::Color::Transparent}
+// 	};
+// 	// _cast = {
+// 	// 	{"rectangle", std::bind(&sfmlFramework::_getRectangle, this, std::placeholders::_1)}
+// 	// };
+// 	_cast.insert(std::make_pair("rectangle", std::bind(&sfmlFramework::_getRectangle, this)));
+// }
+
+// void sfmlFramework::_getRectangle()
+// {
+// 	SfmlSquare *test = dynamic_cast<SfmlSquare *>(_myShape);
+// 	// SfmlSquare *square = dynamic_cast<SfmlSquare *>(_myShape);
+
+// 	// // // _rectangle = square->getShape();
+// 	// _window->draw(square->getShape());
+// 	_window->draw(test->getShape());
+// }
+
+// sfmlFramework::~sfmlFramework()
+// {}
+
+// void sfmlFramework::displayOnWindow()
+// {
+// 	_window->display();
+// }
+
+// void sfmlFramework::destroy()
+// {}
+
+// void sfmlFramework::character()
+// {}
+
+// void sfmlFramework::wall()
+// {}
+
+// void sfmlFramework::score()
+// {}
+
+// void sfmlFramework::time()
+// {}
+
+// void sfmlFramework::characterName()
+// {}
+
+// void sfmlFramework::item()
+// {}
+
+// void sfmlFramework::createWindow(std::size_t x, std::size_t y, const std::string &name)
+// {
+// 	_window = std::make_unique<sf::RenderWindow>(sf::VideoMode(x, y), name);
+// }
+
+// bool sfmlFramework::isOpenWindow() const
+// {
+// 	if (_window->isOpen())
+// 		return true;
+// 	return false;
+// }
+
+// void sfmlFramework::clearWindow()
+// {
+// 	_window->clear();
+// 	_window->pollEvent(_event);
+// }
+
+// void sfmlFramework::destroyWindow()
+// {
+// 	_window->close();
+// }
+
+// bool sfmlFramework::isKeyPressed(std::string c)
+// {
+// 	_strLower(c);
+// 	if (_key.find(c) != _key.end()) {
+// 		if (_window->pollEvent(_event) && sf::Keyboard::isKeyPressed(_key[c])) {
+// 			return true;
+// 		}
+// 	} else // "key doesn't exist in map _key"
+// 		throw std::exception();
+// 	return false;
+// }
+
+// void sfmlFramework::_strLower(std::string &str)
+// {
+// 	for (unsigned int i = 0; i < str.length(); ++i)
+// 		str[i] = tolower(str[i]);
+// }
+
+// IShape *sfmlFramework::_mySquare(const std::size_t x, const std::size_t y, sf::Color color1, sf::Color color2)
+// {
+// 	(void)color2;
+// 	// faire le cpp de sfmlSquare
+// 	// square.setSize(sf::Vector2f(x, y));
+// 	return new SfmlSquare(sf::Vector2f(x, y), color1);
+// }
+
+// IShape *sfmlFramework::drawRectangle(const std::size_t x, const std::size_t y, std::string color1, std::string color2)
+// {
+// 	_strLower(color1);
+// 	_strLower(color2);
+// 	if (_colors.find(color1) != _colors.end()) {
+// 		if (_colors.find(color2) != _colors.end())
+// 			return _mySquare(x, y, _colors[color1], _colors[color2]);
+// 		else
+// 			return _mySquare(x, y, _colors[color1], _colors[color1]);
+// 	} else // color doesn't exist
+// 		throw std::exception();
+// 	// return nullptr;
+// }
+
+
+// // transformer shape dans la forme souhaitée
+// // template<typename T>
+// // ou sinon faire un tableau de poiteur sur fonction de facon a bien caster ..
+// void sfmlFramework::drawInBuff(IShape *shape, std::string type)
+// {
+// 	// SfmlSquare *test = dynamic_cast<SfmlSquare *>(shape);
+
+// 	_myShape = shape;
+
+// 	_cast[type]();
+
+// 	//faire _window->draw dans la function _getRectangle !!!!!!
+// 	// _window->draw(test->getShape());
+// 	// c'est le pointeur sur fonction qui beug
+// }
+
+
 sfmlFramework::sfmlFramework()
-{
-	std::cout << "framwork sfml creating" << std::endl;
-	_key = {
-		{"a", sf::Keyboard::A},
-		{"b", sf::Keyboard::B},
-		{"c", sf::Keyboard::C},
-		{"d", sf::Keyboard::D},
-		{"e", sf::Keyboard::E},
-		{"f", sf::Keyboard::F},
-		{"g", sf::Keyboard::G},
-		{"h", sf::Keyboard::H},
-		{"i", sf::Keyboard::I},
-		{"j", sf::Keyboard::J},
-		{"m", sf::Keyboard::M},
-		{"n", sf::Keyboard::N},
-		{"o", sf::Keyboard::O},
-		{"p", sf::Keyboard::P},
-		{"q", sf::Keyboard::Q},
-		{"r", sf::Keyboard::R},
-		{"s", sf::Keyboard::S},
-		{"t", sf::Keyboard::T},
-		{"u", sf::Keyboard::U},
-		{"v", sf::Keyboard::V},
-		{"w", sf::Keyboard::W},
-		{"x", sf::Keyboard::X},
-		{"y", sf::Keyboard::Y},
-		{"z", sf::Keyboard::Z},
-		{"escape", sf::Keyboard::Escape},
-		{"left", sf::Keyboard::Left},
-		{"right", sf::Keyboard::Right},
-		{"up", sf::Keyboard::Up},
-		{"down", sf::Keyboard::Down}
-	};
-	_colors = {
-		{"black", sf::Color::Black},
-		{"white", sf::Color::White},
-		{"red", sf::Color::Red},
-		{"green", sf::Color::Green},
-		{"blue", sf::Color::Blue},
-		{"yellow", sf::Color::Yellow},
-		{"magenta", sf::Color::Magenta},
-		{"cyan", sf::Color::Cyan},
-		{"transparent", sf::Color::Transparent}
-	};
-	// _cast = {
-	// 	{"rectangle", std::bind(&sfmlFramework::_getRectangle, this, std::placeholders::_1)}
-	// };
-	_cast.insert(std::make_pair("rectangle", std::bind(&sfmlFramework::_getRectangle, this)));
-}
-
-void sfmlFramework::_getRectangle()
-{
-	SfmlSquare *test = dynamic_cast<SfmlSquare *>(_myShape);
-	// SfmlSquare *square = dynamic_cast<SfmlSquare *>(_myShape);
-
-	// // // _rectangle = square->getShape();
-	// _window->draw(square->getShape());
-	_window->draw(test->getShape());
-}
+{}
 
 sfmlFramework::~sfmlFramework()
 {}
 
-void sfmlFramework::displayOnWindow()
-{
-	_window->display();
-}
-
-void sfmlFramework::destroy()
+bool createWindow(std::pair<int, int> size, std::string name)
 {}
 
-void sfmlFramework::character()
+bool clearWindow()
 {}
 
-void sfmlFramework::wall()
+bool createArea(std::pair<int, int> size, std::pair<int, int> pos,
+	std::string name, IGraphic::TYPE type);
 {}
 
-void sfmlFramework::score()
+bool loop(void (*func)(void))
 {}
 
-void sfmlFramework::time()
+std::pair<int, int> getpos(std::string name)
 {}
 
-void sfmlFramework::characterName()
+bool setpos(std::pair<int, int> pos, std::string name)
 {}
 
-void sfmlFramework::item()
+std::pair<int, int> getdim(std::string name)
 {}
 
-void sfmlFramework::createWindow(std::size_t x, std::size_t y, const std::string &name)
-{
-	_window = std::make_unique<sf::RenderWindow>(sf::VideoMode(x, y), name);
-}
+bool setdim(std::pair<int, int> dim, std::string name)
+{}
 
-bool sfmlFramework::isOpenWindow() const
-{
-	if (_window->isOpen())
-		return true;
-	return false;
-}
+bool isKeyPressed(std::string key)
+{}
 
-void sfmlFramework::clearWindow()
-{
-	_window->clear();
-	_window->pollEvent(_event);
-}
-
-void sfmlFramework::destroyWindow()
-{
-	_window->close();
-}
-
-bool sfmlFramework::isKeyPressed(std::string c)
-{
-	_strLower(c);
-	if (_key.find(c) != _key.end()) {
-		if (_window->pollEvent(_event) && sf::Keyboard::isKeyPressed(_key[c])) {
-			return true;
-		}
-	} else // "key doesn't exist in map _key"
-		throw std::exception();
-	return false;
-}
-
-void sfmlFramework::_strLower(std::string &str)
-{
-	for (unsigned int i = 0; i < str.length(); ++i)
-		str[i] = tolower(str[i]);
-}
-
-IShape *sfmlFramework::_mySquare(const std::size_t x, const std::size_t y, sf::Color color1, sf::Color color2)
-{
-	(void)color2;
-	// faire le cpp de sfmlSquare
-	// square.setSize(sf::Vector2f(x, y));
-	return new SfmlSquare(sf::Vector2f(x, y), color1);
-}
-
-IShape *sfmlFramework::drawRectangle(const std::size_t x, const std::size_t y, std::string color1, std::string color2)
-{
-	_strLower(color1);
-	_strLower(color2);
-	if (_colors.find(color1) != _colors.end()) {
-		if (_colors.find(color2) != _colors.end())
-			return _mySquare(x, y, _colors[color1], _colors[color2]);
-		else
-			return _mySquare(x, y, _colors[color1], _colors[color1]);
-	} else // color doesn't exist
-		throw std::exception();
-	// return nullptr;
-}
-
-
-// transformer shape dans la forme souhaitée
-// template<typename T>
-// ou sinon faire un tableau de poiteur sur fonction de facon a bien caster ..
-void sfmlFramework::drawInBuff(IShape *shape, std::string type)
-{
-	// SfmlSquare *test = dynamic_cast<SfmlSquare *>(shape);
-
-	_myShape = shape;
-
-	_cast[type]();
-
-	//faire _window->draw dans la function _getRectangle !!!!!!
-	// _window->draw(test->getShape());
-	// c'est le pointeur sur fonction qui beug
-}
-
-
-
-
-
-
-
-
+bool displayObj()
+{}
